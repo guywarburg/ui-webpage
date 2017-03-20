30a873a6833bf8af0f8f754c48780fbce5d9b1e6
Guy Warburg <guy.warburg@gmail.com>
2017-03-20 15:28:25 +0200

    navbar complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageTextComponent } from './message-text/message-text.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
     MessagesComponent,
-    MessageTextComponent
+    MessageTextComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
