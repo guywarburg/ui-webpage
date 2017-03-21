571b788ae18e868bf5b01fcd1e510c05446b9a8c
Guy Warburg <guy.warburg@gmail.com>
2017-03-21 12:26:57 +0200

    added layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, trigger, state, style, transition, animate } from '@angular/core';
-import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
@@ -21,30 +20,10 @@ import { Hero } from './hero';
   ]
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
-  selectedHero: Hero;
-  heroes = HEROES;
-
+  name = 'John';
   menuState:string = 'out';
 
   toggleSidebar(): void {
     this.menuState = this.menuState === 'out' ? 'in' : 'out';
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 }
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
