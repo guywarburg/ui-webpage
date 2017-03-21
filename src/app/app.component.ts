6e6bce256649de6351e6f54ca0dc08eeb4269153
Guy Warburg <guy.warburg@gmail.com>
2017-03-21 17:51:56 +0200

    side bar works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,9 @@
-import { Component, trigger, state, style, transition, animate } from '@angular/core';
+import { Component, trigger } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('slideInOut', [
-      state('in', style({
-        transform: 'translateX(25%)',
-        width: '80%'
-      })),
-      state('out', style({
-        transform: 'translateX(0)',
-        width: '100%'
-      })),
-      transition('in => out', animate('500ms linear')),
-      transition('out => in', animate('500ms linear'))
-    ]),
-  ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   name = 'John';
